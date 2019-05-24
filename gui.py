46bbca5829bff022fbbcfd929524065254fa2623
s0101y <s0101y@naver.com>
2019-05-24 12:54:52 +0900

    중간시연 - 01

--- gui.py
+++ gui.py
@@ -1,57 +1,50 @@
-#파이참 커밋
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
 
-
-
 Tk = Tk()
-Tk.geometry('800x500+750+200')
+Tk.title("지진 대피소 검색")
+Tk.geometry('1000x500+750+200')
+photo = PhotoImage(file="shel.png")
+Label(Tk, image=photo, height=50, width=50).place(x=20, y=10)
 DataList = []
 
 def IninTopText():
     TempFont = font.Font(Tk, size=30, weight='bold', family='Malgun Gothic')
     MainText = Label(Tk, font=TempFont, text="지진 대피소 검색")
     MainText.pack()
-    MainText.place(x=20)
+    MainText.place(x=80)
 
 def InitInputSi():
-
     global Combobox1
-
     TempFont = font.Font(Tk, size=15, family='Malgun Gothic')
     SiLabel = Label(Tk, font=TempFont, text="시 / 도")
     SiLabel.pack()
-    SiLabel.place(x=50, y=70)
-    #InputLabel = Entry(Tk, font=TempFont, width=10, borderwidth=10, relief='ridge')
-    #InputLabel.pack()
-    #InputLabel.place(x=20, y=100)
-
+    SiLabel.place(x=85, y=70)
     Combobox1 = ttk.Combobox(font=TempFont, textvariable=str, width=10)
     Combobox1['value'] = ("서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도",
                           "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도")    #종류
     Combobox1.current(0)    #시작지점
     Combobox1.pack()
-    Combobox1.place(x=20, y=110)
+    Combobox1.place(x=55, y=110)
 
 def InitInputGu():
     global InputLabel
     TempFont = font.Font(Tk, size=15, family='Malgun Gothic')
     GuLabel = Label(Tk, font=TempFont, text="구")
     GuLabel.pack()
-    GuLabel.place(x=230, y=70)
-    InputLabel = Entry(Tk, font=TempFont, width=10, borderwidth=10, relief='ridge')
+    GuLabel.place(x=265, y=70)
+    InputLabel = Entry(Tk, font=TempFont, width=10, borderwidth=5, relief='ridge')
     InputLabel.pack()
-    InputLabel.place(x=180, y=100)
+    InputLabel.place(x=215, y=110)
 
 def InitSearchButton():
     TempFont = font.Font(Tk, size=12, weight='bold', family='Malgun Gothic')
     SearchButton = Button(Tk, font=TempFont, text='검 색', command=SearchButtonAction)
     SearchButton.pack()
-    SearchButton.place(x=330, y=110)
+    SearchButton.place(x=365, y=110)
 
 def SearchButtonAction():
-
     RenderText.configure(state='normal')
     RenderText.delete(0.0, END)
     SearchLibrary()
@@ -62,21 +55,21 @@ def InitSearchDangerPButton():
     TempFont = font.Font(Tk, size=12, weight='bold', family='Malgun Gothic')
     SearchButton = Button(Tk, font=TempFont, text='근처 지진 취약교량 검색', command=SearchDangerPButtonAction)
     SearchButton.pack()
-    SearchButton.place(x=10, y=160)
+    SearchButton.place(x=110, y=160)
 
 def InitSearchDisasterMsgButton():
     TempFont = font.Font(Tk, size=11, weight='bold', family='Malgun Gothic')
     SearchButton = Button(Tk, font=TempFont, text='현재 장소의 재난알림 검색', command=SearchDisasterMsgButtonAction)
     SearchButton.pack()
-    SearchButton.place(x=10, y=200)
+    SearchButton.place(x=110, y=200)
 
     Label1 = Label(Tk, font=TempFont, text="시설명")
     Label1.pack()
-    Label1.place(x=50, y=235)
+    Label1.place(x=60, y=235)
 
     Label2 = Label(Tk, font=TempFont, text="상세주소")
     Label2.pack()
-    Label2.place(x=235, y=235)
+    Label2.place(x=290, y=235)
 
 
 def SearchLibrary():
@@ -99,15 +92,15 @@ def SearchLibrary():
         print("failed!")
 
     tree = ElementTree.fromstring(strXml)
-    itemElements = tree.getiterator("item")  # item 엘리먼트 리스트 추출
+    itemElements = tree.getiterator("item")     # item 엘리먼트 리스트 추출
 
     for item in itemElements:
-        name = item.find("clnsShuntFcltyNm")  # clnsShuntFcltyNm 검색
+        name = item.find("clnsShuntFcltyNm")    # clnsShuntFcltyNm 검색
         adr = item.find("rdnmadr")              # rdnmadr 검색
-        if len(adr.text) > 0:
-            TEXT.append((name.text, adr.text))
+        if len(adr.text) > 0:                   # 검색된 결과가 있다면
+            TEXT.append((name.text, adr.text))  # 하나의 구호소 이름과 주소를 튜플 타입으로 묶어 리스트 TEXT에 append
 
-    for i in range(len(TEXT)):
+    for i in range(len(TEXT)):                  #RenderText에 삽입
         RenderText.insert(INSERT, "[")
         RenderText.insert(INSERT, i + 1)
         RenderText.insert(INSERT, "] ")
@@ -125,12 +118,11 @@ def SearchDisasterMsgButtonAction():
 
 def InitRenderText():
     global RenderText
-    TempFont = font.Font(Tk, size=10, family='Malgun Gothic')
 
     RenderTextScrollbar = Scrollbar(Tk)
     RenderTextScrollbar.pack()
     RenderTextScrollbar.place(x=375, y=245)
-    RenderText = Text(Tk, width=55, height=17, borderwidth=5, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText = Text(Tk, width=65, height=17, borderwidth=5, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
     RenderText.pack()
     RenderText.place(x=10, y=260)
     RenderTextScrollbar.config(command=RenderText.yview)
